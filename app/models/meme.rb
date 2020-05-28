6e00931297b642d8580a23229a16811105425320
NicolleQuispeArotinco <nicolleqa3@gmail.com>
2020-05-28 17:42:04 -0500

    validation to Meme

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -2,4 +2,7 @@ class Meme < ApplicationRecord
   self.inheritance_column = :_ # Disable STI
   belongs_to :category
   belongs_to :owner
+
+  validates :title, presence: true, uniqueness: true
+  validates :url_source, presence: true, format: { with: /\.(jpg|png|gif)\z/, message: "only allows jpg, png or gif " }
 end
