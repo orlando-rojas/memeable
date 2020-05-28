ded9f9f3f134fe0b2e7ccbac0436f3db7690e2c3
NicolleQuispeArotinco <nicolleqa3@gmail.com>
2020-05-28 16:56:59 -0500

    add other validations

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -2,6 +2,8 @@ class Meme < ApplicationRecord
   belongs_to :category
   belongs_to :owner
 
+  self.inheritance_column = :_ # Disable STI
+
   validates :title, presence: true, uniqueness: true
-  validates :url_source, presence: true
+  validates :url_source, presence: true, format: { with: /\w*(.jpg|.gif|.png)\z/, message: "only allows jpg, png or gif " }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates :username,  presence: true, , uniqueness: true
-  validates :email,  presence: true, , uniqueness: true
+  validates :username,  presence: true, uniqueness: true
+  validates :email,  presence: true, uniqueness: true
 end
