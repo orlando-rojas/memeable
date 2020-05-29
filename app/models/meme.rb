dd7a2ac9044311e64894117f0dd9d762cf3ebec2
orlando-rojas <rojasmoncada.orlando@gmail.com>
2020-05-29 16:19:36 -0500

    seed

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -2,6 +2,10 @@ class Meme < ApplicationRecord
   self.inheritance_column = :_ # Disable STI
   belongs_to :category
   belongs_to :owner, class_name: 'User'
+  has_and_belongs_to_many :tags
+  has_many :votes
+  has_many :voters, through: :votes, source: :user
+  has_many :comments
 
   validates :title, presence: true, uniqueness: true
   validates :url_source, presence: true, format: { with: /\.(jpg|png|gif)\z/, message: "only allows jpg, png or gif " }
