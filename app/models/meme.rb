787f2838ba30649df698bab6d858f3c087aebfd6
Jorge Gonzalez <jorg3.594@gmail.com>
2020-05-28 23:18:09 -0500

    home-order

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,8 +1,13 @@
 class Meme < ApplicationRecord
   self.inheritance_column = :_ # Disable STI
   belongs_to :category
-  belongs_to :owner
+  belongs_to :owner, class_name: 'User'
 
   validates :title, presence: true, uniqueness: true
   validates :url_source, presence: true
+
+  before_create do
+    self.votes_count = 0
+    self.comments_count = 0
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :memes, only: [:show, :new, :create, :edit, :update]
+  get '/popular', to: 'popular#index'
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
