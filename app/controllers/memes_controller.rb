96d731945aaf4b1a18649e66ebc995462be520ff
orlando-rojas <rojasmoncada.orlando@gmail.com>
2020-05-30 10:49:53 -0500

    create meme and footer

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -1,16 +1,30 @@
 class MemesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def show
   end
 
   def new
+    @meme = Meme.new
   end
 
   def create
-  end
+    @meme = current_user.memes.new(meme_params) 
+    if @meme.save
+      redirect_to meme_path(@meme)
+    else
+      render :new
+    end 
+  end 
 
   def edit
   end
 
   def update
   end
+
+  private 
+  def meme_params
+    params.require(:meme).permit(:title, :url_source, :type, :category_id)
+  end 
 end
